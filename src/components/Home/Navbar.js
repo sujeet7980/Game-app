fa9cab686e00636cd2ca8c7133cd74b8155d3b73
Sujeet Kumar <20bcs220@iiitdmj.ac.in>
2023-05-31 15:37:45 +0530

    final commit

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -5,7 +5,11 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-transparent p-3">
     <div  className="container-fluid co" id="navbarSupportedContent">
+      <div>
+        <img src="logo.png" style={{width:"3rem",height:'3rem'}} ></img>
       <Link className="navbar-brand fs-4 text-white">GameGalaxy</Link>
+      </div>
+     
       <div className="ml gap-5 nav-option">
         <Link className="navbar-brand fs-4 text-white">Login</Link>
         <Link className="navbar-brand fs-4 text-white">Sign up</Link>
